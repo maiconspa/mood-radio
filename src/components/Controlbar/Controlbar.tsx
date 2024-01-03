da4d11f1d1fdd8b4ab814a1331aaa5468307f446
Maicon Araujo <maiconsouzapa@gmail.com>
2024-01-03 00:36:41 -0300

    v0.0.3

--- src/components/Controlbar/Controlbar.tsx
+++ src/components/Controlbar/Controlbar.tsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
-import mySound from "../../assets/audio/test.mp3";
 import { ControlbarContainer } from "./Controlbar.styled";
-import { PauseFilled, PlayFilled, SkipBackFilled, SkipForwardFilled } from "@carbon/icons-react";
+import {
+  PauseFilled,
+  PlayFilled,
+  SkipBackFilled,
+  SkipForwardFilled,
+  VolumeDown,
+  VolumeDownFilled,
+  VolumeUpFilled,
+} from "@carbon/icons-react";
+import musics from "../../utils/musics.json";
+
+const AUDIO_FOLDER = "../../assets/audio/";
+const INITIAL_AUDIO = require("../../assets/audio/" + musics[0].name + ".mp3");
+const INITIAL_AUDIO2 = require("../../assets/audio/" + musics[1].name + ".mp3");
+
+console.log(INITIAL_AUDIO)
 
 function Controlbar() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [playSound, soundData] = useSound(mySound);
+  const [volume, setVolume] = useState(0.5);
+  const [musicIndex, setMusicIndex] = useState(0);
+  const [play, { stop }] = useSound(INITIAL_AUDIO, {
+    volume: volume,
+  });
 
   const handlePlay = () => {
     setIsPlaying(!isPlaying);
@@ -14,27 +32,57 @@ function Controlbar() {
 
   useEffect(() => {
     if (isPlaying) {
-      playSound();
+      play();
     } else {
-      soundData.stop();
+      stop();
     }
-  }, [isPlaying, playSound, soundData]);
+  }, [isPlaying, play, stop]);
 
-  return <ControlbarContainer>
-    <div>
+  useEffect(() => {
+    if (musicIndex !== 0) {
+        stop();
+        const next_audio = require("../../assets/audio/" + musics[1].name + ".mp3"); // TODO: arrumar esse cara
+        console.log(INITIAL_AUDIO2)
+        play({id: next_audio})
+    }
+  }, [musicIndex, play, stop])
+
+  const handleVolume = (action: string) => {
+    if (action === "down" && volume > 0) setVolume(volume - 0.1);
+
+    if (action === "up" && volume < 1) setVolume(volume + 0.1);
+  };
+
+  const handleSkipMusic = (action: string) => {
+    if (action === "next" && musics?.[musicIndex + 1])
+      setMusicIndex(musicIndex + 1);
+  };
+
+  return (
+    <ControlbarContainer>
+      <div>
         <button>
           <SkipBackFilled />
         </button>
 
         <button onClick={() => handlePlay()}>
-          {isPlaying ? <PauseFilled /> : <PlayFilled/> }
+          {isPlaying ? <PauseFilled /> : <PlayFilled />}
         </button>
 
-        <button>
+        <button onClick={() => handleSkipMusic("next")}>
           <SkipForwardFilled />
         </button>
+
+        <button onClick={() => handleVolume("down")}>
+          <VolumeDownFilled />
+        </button>
+
+        <button onClick={() => handleVolume("up")}>
+          <VolumeUpFilled />
+        </button>
       </div>
-  </ControlbarContainer>;
+    </ControlbarContainer>
+  );
 }
 
 export default Controlbar;
